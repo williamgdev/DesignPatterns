3fc7c84dd6a8bdcce8593a8f44de47aa8b51f1a1
AndroidStorm <williamg.dev@gmail.com>
2018-02-13 19:47:43 -0800

    Created Subject and Oberver classes

---
+++ app/src/main/java/com/williamgdev/example/designpatterns/Observer.java
@@ -0,0 +1,10 @@
+package com.williamgdev.example.designpatterns;
+
+/**
+ * Created by willimail on 2/13/18.
+ */
+
+public abstract class Observer {
+    protected Subject subject;
+    public abstract void update();
+}
---
+++ app/src/main/java/com/williamgdev/example/designpatterns/Subject.java
@@ -0,0 +1,33 @@
+package com.williamgdev.example.designpatterns;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by willimail on 2/13/18.
+ */
+
+public class Subject {
+    private List<Observer> observers = new ArrayList<>();
+    private int state;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyAllObservers();
+    }
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    private void notifyAllObservers() {
+        for (Observer observer :
+                observers) {
+            observer.update();
+        }
+    }
+}
